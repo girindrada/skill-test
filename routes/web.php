a98e769d897b1f9f0078a39313ae348581254ed7
girindra <gdwiixa@gmail.com>
2026-02-18 12:44:04 +0700

    fix: switch posts routes from api.php to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,6 +13,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('posts', PostController::class)->only(['index', 'show']);
+Route::resource('posts', PostController::class)->middleware('auth')->except(['index', 'show']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
